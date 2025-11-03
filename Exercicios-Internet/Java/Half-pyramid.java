0c17796b760118f90ee9a137b84848c681c4e403
regisdesouza <reginaldo.lima@sptech.school>
2025-11-02 23:11:51 -0300

    Novos exercícios adicionados

---
+++ Exercicios-Internet/Java/Half-pyramid.java
@@ -0,0 +1,12 @@
+Public class main {
+Public static void main (Srting[] args){
+
+  int rows = 5; 
+  for(int = 1; i <= rows; ++i){
+    for( j = 1; j <= 1; ++j){
+      system.out.println(" X ");
+    }
+    system.out.println();
+    }
+  }
+}
